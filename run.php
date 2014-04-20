e5fb2edb9a9cef48db9e0588572fc3954efc6a38
Kadet <kadet1090@gmail.com>
2014-04-20 11:40:40 +0200

    Moved to composer instead of git submodules.

--- run.php
+++ run.php
@@ -5,17 +5,13 @@ else
     define('DEBUG_MODE', 0);
 
 include 'XPBot/functions.php';
-include 'Kadet/Utils/functions.php';
-require 'Kadet/Utils/AutoLoader.php';
+include 'vendor/autoload.php';
 
-$autoloader['Plugins'] = new \Kadet\Utils\AutoLoader('XPBot\\Plugins\\', './Plugins/');
-$autoloader['Plugins']->register();
+$plugins = new \Kadet\Utils\AutoLoader('XPBot\\Plugins\\', './Plugins/');
+$plugins->register();
 
-$autoloader['XPBot'] = new \Kadet\Utils\AutoLoader('XPBot\\', './XPBot/');
-$autoloader['XPBot']->register();
-
-$autoloader['Kadet'] = new \Kadet\Utils\AutoLoader('Kadet\\', './Kadet/');
-$autoloader['Kadet']->register();
+$autoloader = new \Kadet\Utils\AutoLoader('XPBot\\', './XPBot/');
+$autoloader->register();
 
 $client = new \XPBot\Bot();
 $client->connect();
